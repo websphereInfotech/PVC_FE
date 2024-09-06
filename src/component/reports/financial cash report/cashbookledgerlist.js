c5920be9ef2792593b39b703e664a8e46e1c4083
websphereInfotech <wsinfo08317@gmail.com>
2024-09-06 11:54:41 +0530

    changed UI of Cashbook ledger

--- src/component/reports/financial cash report/cashbookledgerlist.js
+++ src/component/reports/financial cash report/cashbookledgerlist.js
@@ -1,20 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Table, TableBody, TableRow, TableCell, Card, TableHead, Grid, useMediaQuery, useTheme } from '@mui/material';
+import { Typography, Table, TableBody, TableRow, TableCell, Card, TableHead, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getallCashbookledger } from 'store/thunk';
 
 const formatDate = (dateString) => {
+  if (!dateString) {
+    console.error('Date string is undefined or empty:', dateString);
+    return 'Invalid Date';
+  }
   const options = { day: 'numeric', month: 'short', year: 'numeric' };
-  return new Date(dateString).toLocaleDateString('en-GB', options);
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    console.error('Invalid date format:', dateString);
+    return 'Invalid Date';
+  }
+
+  return date.toLocaleDateString('en-GB', options);
 };
 
 const Cashbookledgerlist = () => {
   const dispatch = useDispatch();
-  const theme = useTheme();
 
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
-  const [payments, setPayments] = useState([]);
+  const [payments, setPayments] = useState({});
   const [getdata, setGetdata] = useState({});
   const formData = sessionStorage.getItem('RCashbookformDate');
   const toDate = sessionStorage.getItem('RCashbooktoDate');
@@ -22,46 +29,24 @@ const Cashbookledgerlist = () => {
   useEffect(() => {
     dispatch(getallCashbookledger(formData, toDate))
       .then((data) => {
-        if (data && data.records) {
-          setPayments(data);
-          setGetdata(data.form);
-        } else {
-          console.error('Data or data.records is undefined.');
-        }
+        setPayments(data.records || {});
+        setGetdata(data.form);
       })
       .catch((error) => {
         console.error('Error fetching payment ledger data:', error);
       });
   }, [dispatch, formData, toDate]);
 
-  return (
-    <Card style={{ width: '100%', padding: '25px' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6">From:</Typography>
-          <Typography variant="h4">{getdata.companyname}</Typography>
-          <Typography>{getdata.address1}</Typography>
-          <Typography>
-            {getdata.city}, {getdata.state}, {getdata.pincode}
-          </Typography>
-        </Grid>
+  const renderRecordsByDate = (date, data) => {
+    const { totals, closingBalance, records } = data;
+    return (
+      <Grid container spacing={2} key={date} style={{ marginBottom: '20px' }}>
         <Grid item xs={12}>
-          <Typography variant="h6" align="center">
-            {formatDate(formData)} to {formatDate(toDate)}
+          <Typography variant="h5" align="center" gutterBottom>
+            Date: {formatDate(date)}
           </Typography>
         </Grid>
-      </Grid>
-
-      <Grid container style={{ marginTop: '20px', overflowY: 'scroll' }}>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{
-            borderRight: { xs: 'none', md: '1px solid #ccc' },
-            paddingRight: { md: '16px' }
-          }}
-        >
+        <Grid item xs={12} md={6}>
           <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
             Credit
           </Typography>
@@ -75,27 +60,28 @@ const Cashbookledgerlist = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {payments?.records
-                ?.filter((entry) => entry.creditAmount)
-                .map((entry, index) => (
-                  <TableRow key={index}>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.creditAmount}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
-                  </TableRow>
-                ))}
-              {payments?.totals && (
+              {records &&
+                records
+                  .filter((entry) => entry.creditAmount > 0)
+                  .map((entry, index) => (
+                    <TableRow key={index}>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.creditAmount}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
+                    </TableRow>
+                  ))}
+              {totals && (
                 <TableRow>
                   <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                    Total Credit:{payments?.totals?.totalCredit}
+                    Total Credit: {totals.totalCredit}
                   </TableCell>
                 </TableRow>
               )}
-              {payments?.closingBalance && payments?.closingBalance.type === 'credit' && (
+              {closingBalance && closingBalance.type === 'credit' && (
                 <TableRow>
                   <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                    Closing Balance:{payments?.closingBalance?.amount}
+                    Closing Balance: {closingBalance.amount}
                   </TableCell>
                 </TableRow>
               )}
@@ -103,8 +89,8 @@ const Cashbookledgerlist = () => {
           </Table>
         </Grid>
 
-        <Grid item xs={12} md={6} sx={{ paddingLeft: { md: '16px' } }}>
-          <Typography variant="h4" gutterBottom style={{ textAlign: 'center', marginTop: isSmallScreen ? '20px' : '0px' }}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
             Debit
           </Typography>
           <Table>
@@ -117,33 +103,53 @@ const Cashbookledgerlist = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {payments?.records
-                ?.filter((entry) => entry.debitAmount)
-                .map((entry, index) => (
-                  <TableRow key={index} style={{ textAlign: 'center' }}>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.debitAmount}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
-                    <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
-                  </TableRow>
-                ))}
-              {payments?.totals && (
+              {records &&
+                records
+                  .filter((entry) => entry.debitAmount > 0)
+                  .map((entry, index) => (
+                    <TableRow key={index}>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.debitAmount}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
+                    </TableRow>
+                  ))}
+              {totals && (
                 <TableRow>
                   <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                    Total Debit:{payments?.totals?.totalDebit}
+                    Total Debit: {totals.totalDebit}
                   </TableCell>
                 </TableRow>
               )}
-              {payments?.closingBalance && payments?.closingBalance.type === 'debit' && (
+              {closingBalance && closingBalance.type === 'debit' && (
                 <TableRow>
                   <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                    Closing Balance:{payments?.closingBalance?.amount}
+                    Closing Balance: {closingBalance.amount}
                   </TableCell>
                 </TableRow>
               )}
             </TableBody>
           </Table>
         </Grid>
+      </Grid>
+    );
+  };
+
+  return (
+    <Card style={{ width: '100%', padding: '25px' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6">From:</Typography>
+          <Typography variant="h4">{getdata.companyname}</Typography>
+          <Typography>{getdata.address1}</Typography>
+          <Typography>
+            {getdata.city}, {getdata.state}, {getdata.pincode}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container style={{ marginTop: '20px', overflowY: 'scroll' }}>
+        {Object.entries(payments).map(([date, data]) => renderRecordsByDate(date, data))}
       </Grid>
     </Card>
   );
--- src/views/finacial managenment/Wallet/cashbook.js
+++ src/views/finacial managenment/Wallet/cashbook.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
-import { Button, Grid, Typography, Table, TableBody, TableRow, TableCell, TableHead, useMediaQuery, Paper } from '@mui/material';
+import { Button, Grid, Typography, Table, TableBody, TableRow, TableCell, TableHead, Paper } from '@mui/material';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useDispatch } from 'react-redux';
-import { useTheme } from '@emotion/react';
 import { getallCashbookledger } from 'store/thunk';
 
+const formatDate = (dateString) => {
+  if (!dateString) {
+    console.error('Date string is undefined or empty:', dateString);
+    return 'Invalid Date';
+  }
+  const options = { day: 'numeric', month: 'short', year: 'numeric' };
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    console.error('Invalid date format:', dateString);
+    return 'Invalid Date';
+  }
+
+  return date.toLocaleDateString('en-GB', options);
+};
 const CashbookReport = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const dispatch = useDispatch();
 
   const [toDate, setToDate] = useState(new Date());
@@ -28,16 +39,11 @@ const CashbookReport = () => {
   const handleLedger = (formDate, toDate) => {
     const formattedFormDate = formatDateForApi(formDate);
     const formattedToDate = formatDateForApi(toDate);
-
     dispatch(getallCashbookledger(formattedFormDate, formattedToDate))
       .then((data) => {
-        if (data && data.records) {
-          setPayments(data);
-          setGetdata(data.form);
-          setShowData(true);
-        } else {
-          console.error('Data or data.records is undefined.');
-        }
+        setPayments(data.records || {});
+        setGetdata(data.form);
+        setShowData(true);
       })
       .catch((error) => {
         console.error('Error fetching payment ledger data:', error);
@@ -51,11 +57,103 @@ const CashbookReport = () => {
     return `${year}-${month}-${day}`;
   };
 
-  const formatDateForDisplay = (dateString) => {
-    const options = { day: 'numeric', month: 'short', year: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-GB', options);
-  };
+  const renderRecordsByDate = (date, data) => {
+    const { totals, closingBalance, records } = data;
+    return (
+      <Grid container spacing={2} key={date} style={{ marginBottom: '20px' }}>
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center" gutterBottom>
+            Date: {formatDate(date)}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
+            Credit
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Amount</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Name</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Particulars</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {records &&
+                records
+                  .filter((entry) => entry.creditAmount > 0)
+                  .map((entry, index) => (
+                    <TableRow key={index}>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.creditAmount}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
+                    </TableRow>
+                  ))}
+              {totals && (
+                <TableRow>
+                  <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
+                    Total Credit: {totals.totalCredit}
+                  </TableCell>
+                </TableRow>
+              )}
+              {closingBalance && closingBalance.type === 'credit' && (
+                <TableRow>
+                  <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
+                    Closing Balance: {closingBalance.amount}
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Grid>
 
+        <Grid item xs={12} md={6}>
+          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
+            Debit
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Amount</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Name</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Particulars</TableCell>
+                <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {records &&
+                records
+                  .filter((entry) => entry.debitAmount > 0)
+                  .map((entry, index) => (
+                    <TableRow key={index}>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.debitAmount}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
+                      <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
+                    </TableRow>
+                  ))}
+              {totals && (
+                <TableRow>
+                  <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
+                    Total Debit: {totals.totalDebit}
+                  </TableCell>
+                </TableRow>
+              )}
+              {closingBalance && closingBalance.type === 'debit' && (
+                <TableRow>
+                  <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
+                    Closing Balance: {closingBalance.amount}
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Grid>
+      </Grid>
+    );
+  };
   return (
     <Paper elevation={4} style={{ padding: '24px' }}>
       <Typography variant="h4" align="center" gutterBottom id="mycss">
@@ -88,97 +186,10 @@ const CashbookReport = () => {
                 {getdata.city}, {getdata.state}, {getdata.pincode}
               </Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6" align="center">
-                {formatDateForDisplay(formDate)} to {formatDateForDisplay(toDate)}
-              </Typography>
-            </Grid>
           </Grid>
 
           <Grid container style={{ marginTop: '20px', overflowY: 'scroll' }}>
-            <Grid item xs={12} md={6} sx={{ borderRight: { xs: 'none', md: '1px solid #ccc' }, paddingRight: { md: '16px' } }}>
-              <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
-                Credit
-              </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Amount</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Name</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Particulars</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Date</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {payments?.records
-                    ?.filter((entry) => entry.creditAmount)
-                    .map((entry, index) => (
-                      <TableRow key={index}>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.creditAmount}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
-                      </TableRow>
-                    ))}
-                  {payments?.totals && (
-                    <TableRow>
-                      <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                        Total Credit: {payments?.totals?.totalCredit}
-                      </TableCell>
-                    </TableRow>
-                  )}
-                  {payments?.closingBalance && payments?.closingBalance.type === 'credit' && (
-                    <TableRow>
-                      <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                        Closing Balance: {payments?.closingBalance?.amount}
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Grid>
-
-            <Grid item xs={12} md={6} sx={{ paddingLeft: { md: '16px' } }}>
-              <Typography variant="h4" gutterBottom style={{ textAlign: 'center', marginTop: isMobile ? '20px' : '0px' }}>
-                Debit
-              </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Amount</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Name</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Particulars</TableCell>
-                    <TableCell style={{ fontWeight: 'bold', textAlign: 'center' }}>Date</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {payments?.records
-                    ?.filter((entry) => entry.debitAmount)
-                    .map((entry, index) => (
-                      <TableRow key={index} style={{ textAlign: 'center' }}>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.debitAmount}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.personName}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{entry.details}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>{new Date(entry.date).toLocaleDateString('en-GB')}</TableCell>
-                      </TableRow>
-                    ))}
-                  {payments?.totals && (
-                    <TableRow>
-                      <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                        Total Debit: {payments?.totals?.totalDebit}
-                      </TableCell>
-                    </TableRow>
-                  )}
-                  {payments?.closingBalance && payments?.closingBalance.type === 'debit' && (
-                    <TableRow>
-                      <TableCell colSpan={4} style={{ textAlign: 'start', fontWeight: 'bold' }}>
-                        Closing Balance: {payments?.closingBalance?.amount}
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Grid>
+            {Object.entries(payments).map(([date, data]) => renderRecordsByDate(date, data))}
           </Grid>
         </>
       )}
