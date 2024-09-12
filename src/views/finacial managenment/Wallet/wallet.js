2f9c4a7cd7d40b01d3797d18bbe9bad625447820
websphereInfotech <wsinfo08317@gmail.com>
2024-09-12 14:43:13 +0530

    api in wallet change

--- src/views/finacial managenment/Wallet/wallet.js
+++ src/views/finacial managenment/Wallet/wallet.js
@@ -79,6 +79,7 @@ const Wallet = () => {
         } else {
           const newdata = await dispatch(getWallet('me', formDate, toDate, navigate));
           setWalletData(newdata);
+          console.log(newdata);
         }
       } catch (error) {
         console.log('fetch data of wallet:', error);
